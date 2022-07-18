ee8a2e0eb75ae38ae1a6e526fa5f9a4ec8e162ae
Carlos Mejia <cmejia@equitel.com.co>
2022-07-18 06:18:38 -0500

    export variable and aliases of imports

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-import { secret_number } from "./math";
+import { secret_number, add as sumar, subtract, divide, name } from "./math.js";
 
-console.log(secret_number);
+function add(a, b) {
+  return a + b;
+};
+
+console.log(add(100, 20));
+console.log(sumar(100, 20));
--- math.js
+++ math.js
@@ -1,5 +1,19 @@
 export const secret_number = 30;
+//export variable
+export let name = "My math module";
 
-function add(x, y) {
-    return x + y;
+export function add(x, y) {
+  return x + y;
+}
+
+export function subtract(x, y) {
+  return x - y;
+}
+
+export function multiply(x, y) {
+  return x * y;
+}
+
+export function divide(x, y) {
+  return x / y;
 }
