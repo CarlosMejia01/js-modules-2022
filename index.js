7ba4a3085d7d8d71e1230b796a61a625afa4ba12
Carlos Mejia <cmejia@equitel.com.co>
2022-07-18 09:21:55 -0500

    export all

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import math from "./math.js";
+import * as math from "./math.js";
+
+console.log(add(10, 100));
+console.log(subtract(100, 100));
 
 console.log(math.add(10,100));
 console.log(math.subtract(100,100));
